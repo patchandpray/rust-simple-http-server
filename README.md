53a08a43ec9c14714fc275f45d696b942361c7e4
tim.vandekeer <t.van.de.keer@gmail.com>
2020-11-02 15:24:32 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# rust-simple-http-server
---
+++ src/server.rs
@@ -0,0 +1,35 @@
+use std::io::Read;
+use std::net::TcpListener;
+
+pub struct Server {
+    addr: String,
+}
+
+impl Server {
+    pub fn new(addr: String) -> Self {
+        Self { addr }
+    }
+
+    pub fn run(self) {
+
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        println!("Listening on {}", self.addr);
+
+        loop {                
+            match listener.accept() {
+                Ok((mut stream, addr)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                        },
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
+                },
+                Err(e) => println!("Failed to establish a connection: {}", e),
+            }
+        }
+
+    }
+}
