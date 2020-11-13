85ef0b6e08370e65f8fbba769779caeeba7a4174
tim.vandekeer <t.van.de.keer@gmail.com>
2020-11-13 08:57:04 +0100

    adds public path and path matching for request returning response

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,11 +1,26 @@
 use super::server::Handler;
-use super::http::{Request, Response, StatusCode};
+use super::http::{Request, Response, StatusCode, Method};
 
 #[derive(Debug)]
-pub struct WebsiteHandler;
+pub struct WebsiteHandler {
+    public_path: String,
+}
+
+impl WebsiteHandler {
+    pub fn new(public_path: String) -> Self {
+        Self { public_path }
+    }
+}
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
-        Response::new(StatusCode::Ok, Some("<h1>TEST</h1>"))
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>")),
+                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>")),
+                _ => Response::new(StatusCode::NotFound, None)
+            }
+            _ => Response::new(StatusCode::NotFound, None)
+        }
     }
 }
